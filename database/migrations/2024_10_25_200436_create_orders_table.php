b2e2ff10706305c5fe176cd275b19b271155302b
aguconco <agustinconcollato@gmail.com>
2024-11-01 16:08:35 -0300

    cambios en la base de datos - tabla orders

--- database/migrations/2024_10_25_200436_create_orders_table.php
+++ database/migrations/2024_10_25_200436_create_orders_table.php
@@ -4,19 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->string('order_id', 25);
-            $table->string('client', 100);
+            $table->string('id', 25);
             $table->string('client_id', 25);
-            $table->string('state_order', 25);
+            $table->string('status', 25);
+            $table->string('comment', 200);
             $table->bigInteger('date');
+            $table->decimal('total_amount', 10, 2)->default(0);
+            $table->string('payment_type', 25)->default('efectivo');
             $table->integer('count', true);
         });
     }
