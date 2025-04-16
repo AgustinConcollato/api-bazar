07d44e36e6da60358bee8636320dbd9b1f3d46f2
Agustín Concollato <agustinconcollato@gmail.com>
2025-04-16 11:18:03 -0300

    Add ClientAddressController and UserController for address management and user authentication

---
+++ app/Http/Controllers/ClientAddressController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClientAddress;
+use Illuminate\Http\Request;
+
+class ClientAddressController
+{
+    public function get($userId)
+    {
+
+        $user = ClientAddress::where('client_id', $userId)->get();
+
+        if (!$user) {
+            return response()->json(['status' => 'error', 'message' => 'No hay direcciones registradas'], 404);
+        }
+
+        return response()->json($user);
+    }
+
+    public function add(Request $request)
+    {
+
+        $data = $request->validate([
+            'zip_code' => 'nullable|string',
+            'status' => 'nullable|string',
+            'user_id' => 'required|string',
+            'province' => 'required|string',
+            'city' => 'required|string',
+            'address' => 'required|string',
+            'code' => 'required|string',
+            'address_number' => 'required|string',
+        ]);
+
+        $address = ClientAddress::create($data);
+
+        return response()->json($address, 201);
+    }
+    public function update(Request $request, $userId)
+    {
+        $code = $request->input('code');
+
+        $address = ClientAddress::where('client_id', $userId)
+            ->where('status', 'selected')
+            ->first();
+
+        $address->update(['status' => null]);
+
+        $newAddress = ClientAddress::where('client_id', $userId)
+            ->where('code', $code)
+            ->first();
+
+        $newAddress->update(['status' => 'selected']);
+
+        $addresses = ClientAddress::where('client_id', $userId)->get();
+
+        return response()->json($addresses);
+    }
+}
