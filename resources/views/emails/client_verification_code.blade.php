65403ab69fd5e9896f4fdcb9590da7cc5bd7e8fc
Agustín Concollato <agustinconcollato@gmail.com>
2025-04-28 15:22:38 -0300

    Add client verification code email template

---
+++ resources/views/emails/client_verification_code.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Código de verificación</title>
+</head>
+
+<body>
+    <h3>¡Hola! {{ $name }}</h3>
+    <p>Tu código de verificación es:</p>
+    <h2>{{ $code }}</h2>
+    <p>Copialo y pegalo en la página para verificar tu cuenta.</p>
+</body>
+
+</html>
