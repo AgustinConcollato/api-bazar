968e3a68b61fa7fba719d076df7d5b4cc9d30b43
Agustín Concollato <agustinconcollato@gmail.com>
2025-05-27 00:30:11 -0300

    Add cash register entry creation to PaymentService for payment processing
    Enhance PaymentService by injecting CashRegisterService and implementing cash register entry creation upon payment creation and updates. This ensures accurate tracking of cash transactions related to payments.

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -7,6 +7,13 @@ use Carbon\Carbon;
 
 class PaymentService
 {
+    protected $cashRegisterService;
+
+    public function __construct(CashRegisterService $cashRegisterService)
+    {
+        $this->cashRegisterService = $cashRegisterService;
+    }
+
     public function createPayment($validated)
     {
         $payment = Payment::create([
@@ -16,6 +23,11 @@ class PaymentService
             'paid_amount' => $validated['paid_amount'] ?? null,
             'paid_at' => $validated['paid_at'] ?? null,
         ]);
+
+        // Create cash register entry if payment is paid
+        if (isset($validated['paid_amount']) && isset($validated['paid_at'])) {
+            $this->createCashRegisterEntry($payment);
+        }
 
         return $payment;
     }
@@ -35,6 +47,32 @@ class PaymentService
         $data['paid_at'] = Carbon::now();
         $payment = Payment::find($id);
         $payment->update($data);
+
+        // Create cash register entry if payment is being marked as paid
+        if (isset($data['paid_amount'])) {
+            $this->createCashRegisterEntry($payment);
+        }
+
         return $payment;
+    }
+
+    private function createCashRegisterEntry($payment)
+    {
+        // Obtener el pedido y sus productos
+        $order = $payment->order;
+        $orderProducts = $order->products;
+
+        // Calcular el costo total basado en purchase_price
+        $totalCost = $orderProducts->sum(function ($product) {
+            return $product->purchase_price * $product->quantity;
+        });
+
+        $this->cashRegisterService->deposit([
+            'method' => $payment->method,
+            'amount' => $totalCost,
+            'type' => 'in',
+            'description' => 'Pago del pedido de ' . $order->client_name,
+            'payment_id' => $payment->id
+        ]);
     }
 }
